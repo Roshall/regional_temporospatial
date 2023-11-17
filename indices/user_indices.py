56775936d0f21d5894769ef18e5b1d5b56639800
Roshall <15504658671@163.com>
2023-11-17 11:07:58 +0800

    update user index

--- indices/user_indices.py
+++ indices/user_indices.py
@@ -2,17 +2,17 @@ from functools import partial
 
 import btree
 
-from .region import GridRegion
-from .two_level_merge_index import TwoLevelMergeIndex, fuzzy
+from .region import GridRegion, unsure
+from .two_level_merge_index import TwoLevelMergeIndex
 
 # Here we need duration to deduct the correct beginning of a segment
 # overlapping within the searching interval. Therefore, fuzzy search is needed.
 # key: (duration, begin)
-Tempo2DIdx = partial(fuzzy(TwoLevelMergeIndex), btree.BtreeMap, btree.BtreeMultiMap)
+Tempo2DIdx = partial(TwoLevelMergeIndex, btree.BtreeMap, btree.BtreeMultiMap, 'FuzzyInnerAll')
 # key: (duration, (x, y))
-Out3DRegion = partial(TwoLevelMergeIndex, GridRegion, btree.BtreeMap)
+Out3DRegion = partial(TwoLevelMergeIndex, unsure(GridRegion), btree.BtreeMap, 'NotSureSearch')
 # key:((duration, (x, y)), (duration, begin))
-UserIdx = partial(TwoLevelMergeIndex, Out3DRegion, Tempo2DIdx)
+UserIdx = partial(TwoLevelMergeIndex, Out3DRegion, Tempo2DIdx, 'NotSureSearch')
 
 
 
