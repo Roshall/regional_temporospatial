ff813daf016ea0bdf9789b26c0ce2c34db5a3f5e
Roshall <15504658671@163.com>
2023-11-30 17:02:57 +0800

    tow function tests passed.

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
+import heapq
+import math
+from operator import attrgetter
+
 import numpy as np
 
 from indices import UserIdx
 from indices.region import GridRegion
-from utilities.trajectory import NaiveTrajectorySeg
+from utilities.box2D import Box2D
+from utilities.trajectory import NaiveTrajectorySeg, RunTimeTrajectorySeg
 
 
 def build_tempo_spatial_index(trajs):
@@ -40,6 +45,77 @@ def build_tempo_spatial_index(trajs):
     return user_idx
 
 
-def search(tempo_spat_idx, region, labels, duration_range, interval):
+def candidate_verified_queue(region, candidates, interval, buffer_size: int = 1024):
+    cand_queue = heapq.merge(*candidates)
+    verified_heap = []
+    # build queue
+    for cand_seg in cand_queue:
+        verified_heap.extend(verify_seg(region, cand_seg, interval))
+        if len(verified_heap) >= buffer_size:
+            break
+    heapq.heapify(verified_heap)
+
+    for cand_seg in cand_queue:
+        verified_seg = verify_seg(region, cand_seg, interval)
+        for seg in verified_seg:
+            yield heapq.heapreplace(verified_heap, seg)
+
+    while verified_heap:
+        yield heapq.heappop(verified_heap)
+
+
+def verify_seg(region, segment: NaiveTrajectorySeg, interval):
+    mask = region.enclose(segment.points)
+    break_pos = np.append(-1, np.where(mask == False))
+    seg_lens = np.diff(break_pos, append=len(mask)) - 1
+    # if the seg is cut into pieces, each is treated separately
+    seg_lens_mid = seg_lens[1:-1]
+    seg_lens_mid[seg_lens_mid < interval] = 0
+
+    res_seg = [RunTimeTrajectorySeg(segment.id, segment.begin + pos + 1, len_)
+               for pos, len_ in zip(break_pos, seg_lens) if len_ > 0]
+    return res_seg
+
+
+def search(tempo_spat_idx, region: Box2D, labels, duration_range, interval):
     # FIXME: only rectangle region
-    candidates = tempo_spat_idx.where_intersect(((duration_range, region), interval))
+    # TODO: ADD labels
+    candidates, probation = tempo_spat_idx.perhaps_intersect(((region.bbox, duration_range), interval))
+    traj_queue = heapq.merge(*probation, candidate_verified_queue(region, candidates, interval), key=attrgetter('begin'))
+    end_queue = []
+    next_end = math.inf
+    active_space = {}
+    pre_insert = {}
+    new_ = -1
+
+    for traj in traj_queue:
+        begin = traj.begin
+        if new_ == begin:
+            assert traj.id in pre_insert
+            pre_insert[traj.id] = traj
+        else:
+            new_ = begin
+            if next_end == new_:
+                while next_end == end_queue[0][0]:
+                    end, tra_id = heapq.heappop(end_queue)
+                    if tra_id in pre_insert:
+                        del pre_insert[tra_id]
+                    elif end - active_space[tra_id] < interval:
+                        del active_space[tra_id]
+                    else:
+                        # produce result
+                        pass
+            for tra_id, tra in pre_insert.items():
+                assert tra_id in active_space
+                active_space[tra_id] = tra.begin
+                heapq.heappush(end_queue, (tra.len + tra.begin, tra_id))
+            pre_insert.clear()
+            next_end = end_queue[0][0]
+
+    while end_queue:
+        end, tra_id = heapq.heappop(end_queue)
+        if end - active_space[tra_id] < interval:
+            del active_space
+        else:
+            # produce result
+            pass
--- test/test_run.py
+++ test/test_run.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
 
-from run import build_tempo_spatial_index
+from run import build_tempo_spatial_index, verify_seg, candidate_verified_queue
+from utilities.box2D import Box2D
 from utilities.config import config
 from utilities.data_preprocessing import load_data, gen_border
-from utilities.trajectory import NaiveTrajectorySeg
+from utilities.trajectory import NaiveTrajectorySeg, RunTimeTrajectorySeg
 
 
 class TestBuildTempSpatialIndex:
@@ -26,3 +27,34 @@ class TestBuildTempSpatialIndex:
                         assert (seg == ans).all()
                     case _:
                         assert False
+
+
+def test_verify_seg():
+    region = Box2D((11, 111, 11, 111))
+    # start and end are out
+    points1 = np.array([[10, 67], [13, 83], [19, 92], [23, 113], [41, 121], [59, 102], [81, 83], [113, 75], [101, 49],
+                        [54, 19], [31, 7], [27, 27], [19, 45], [13, 51], [9, 55]])
+    seg1 = NaiveTrajectorySeg(1, 0, points1)
+    res = verify_seg(region, seg1, 3)
+    assert len(res) == 1
+    assert res == [RunTimeTrajectorySeg(1, 11, 3)]
+    res = verify_seg(region, seg1, 2)
+    assert len(res) == 4
+    assert [seg.begin for seg in res] == [1, 5, 8, 11]
+
+    seg2 = NaiveTrajectorySeg(1, 0, points1[1:-1])
+    res = verify_seg(region, seg2, 3)
+    assert len(res) == 2
+    assert [(seg.begin, seg.len) for seg in res] == [(0, 2), (10, 3)]
+
+
+def test_candidate_verified_queue():
+    x = np.linspace(0, np.pi, 100)
+    Y = [np.cos(x*20) * 10, np.sin((x + np.pi/4)*20) * 10]
+    all_points = [np.vsplit(np.vstack((x, y)).T, 10) for y in Y]
+    cand = [[NaiveTrajectorySeg(i, j*10, obj_ps) for j, obj_ps in enumerate(ind)] for i, ind in enumerate(all_points)]
+
+    region = Box2D((0, 1.5 * np.pi, 0, 10))
+    data = list(candidate_verified_queue(region, cand, 5, 10))
+    assert len(list(data)) == 30
+
