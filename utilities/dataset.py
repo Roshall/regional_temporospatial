b9dc186e6c3f5b26d35e00c974bf1d7c5b5e3608
Roshall <15504658671@163.com>
2023-12-15 16:00:07 +0800

    I'm more familiar with pandas.

--- utilities/dataset.py
+++ utilities/dataset.py
@@ -7,16 +7,12 @@ def load_rounD(path, track_id):
     with ZipFile(f'{path}/rounD.zip') as zf:
         prefix = f'rounD/data/{track_id}_tracks'
         with zf.open(f'{prefix}Meta.csv') as f:
-            meta = pd.read_csv(f)
+            meta = pd.read_csv(f, index_col='trackId', usecols=['trackId', 'class'])
 
         with zf.open(f'{prefix}.csv') as f:
             data = pd.read_csv(f)
-    desired_cols = ['trackId, class']
-    class_map = meta[desired_cols]
     label = ('car', 'truck', 'van', 'bus', 'trailer', 'bicycle', 'motorcycle', 'pedestrian')
-    label_map = dict((l, i) for i, l in enumerate(label))
-    class_map = class_map['class'].transform(lambda x: label_map[x])
-    class_map = {i: row['class'] for i, row in class_map.iterrows()}
+    class_map = meta['class'].replace({l: i for i, l in enumerate(label)})
     return class_map, data
 
 
