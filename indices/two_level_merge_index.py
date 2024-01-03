bd11b02c8a08c35473b111663a7d40be72484f45
Roshall <15504658671@163.com>
2024-01-03 09:51:23 +0800

    strategy pattern with functions.

--- indices/two_level_merge_index.py
+++ indices/two_level_merge_index.py
@@ -1,12 +1,12 @@
-import sys
 from abc import ABCMeta, abstractmethod
+from functools import partial
 
 
 class TwoLevelMergeIndex:
     def __init__(self, outer_cls, inner_cls, intersect_strategy='ReturnEntry'):
         self._outer = outer_cls()
         self._inner_cls = inner_cls
-        self.where_intersect = strategies[intersect_strategy](self._outer)
+        self.where_intersect = partial(strategies[intersect_strategy], self._outer)
 
     def add(self, key, entry) -> None:
         if (inner_idx := self._outer.where_contain(key[0])) is None:
@@ -18,75 +18,50 @@ class TwoLevelMergeIndex:
         return (inner := self._outer.where_contain(key[0])) and inner.where_contain(key[1])
 
 
-class IntersectStrategy(metaclass=ABCMeta):
-    def __init__(self, outer_idx):
-        self._outer = outer_idx
-
-    @abstractmethod
-    def __call__(self, bboxes):
-        """
-        return items according to a concrete strategy
-        :param bboxes: (dim0_min, dim0_max, dim1_min, dim1_max, ...)
-        :return: iterable itemes
-        """
-        pass
-
-
-class IntersectReturnEntry(IntersectStrategy):
+def intersect_return_entry(outer_idx, bboxes):
     """
-    get inner entries one by one
+    :param outer_idx: the top level index.
+    :param bboxes: (dim0_min, dim0_max, dim1_min, dim1_max, ...).
+    :return: a generator that simply searches respecting $bboxes
     """
-    def __init__(self, outer_idx):
-        super().__init__(outer_idx)
-
-    def __call__(self, bboxes):
-        for inner_idx in self._outer.where_intersect(bboxes[0]):
-            yield from inner_idx.where_intersect(bboxes[1])
+    for inner_idx in outer_idx.where_intersect(bboxes[0]):
+        yield from inner_idx.where_intersect(bboxes[1])
 
 
-class IntersectFuzzySearch(IntersectStrategy):
+def intersect_fuzzy_search(outer_idx, bboxes):
     """
-    linear constrains search
+    linear constrains search.
+    :param outer_idx: the top level index
+    :param bboxes: just care (dim2_min, dim2_max).
+    :return: a generator that searches all possible region.
     """
-    def __init__(self, outer_idx):
-        super().__init__(outer_idx)
-
-    def __call__(self, bboxes):
-        """
-        :param bboxes: just care (dim2_min, dim2_max)
-        :return: all possible region, iterable.
-        """
-        lo, hi = bboxes
-        for outer_key, inner_idx in self._outer:
-            yield from inner_idx.where_intersect((lo - outer_key + 1, hi))
+    lo, hi = bboxes
+    for outer_key, inner_idx in outer_idx:
+        yield from inner_idx.where_intersect((lo - outer_key + 1, hi))
 
 
-class IntersectNotSureSearch(IntersectStrategy):
+def intersect_not_sure_search(outer_idx, bboxes):
     """
-    separate absolute inner entries from the unsure
-    return iterable tuple of generators: (not_sure, sure)
+    separate absolute inner entries from the unsure.
+    :param outer_idx: the top level index.
+    :param bboxes: (dim0_min, dim0_max, dim1_min, dim1_max, ...).
+    :return a tuple of generators: (not_sure, sure)
     """
-    def __init__(self, outer_idx):
-        super().__init__(outer_idx)
-
-    def __call__(self, bboxes):
-        candidates, probation = self._outer.where_intersect(bboxes[0])
-        return (entry for inner in candidates for entry in inner.where_intersect(bboxes[1])), \
-            (entry for inner in probation for entry in inner.where_intersect(bboxes[1]))
+    candidates, probation = outer_idx.where_intersect(bboxes[0])
+    return (entry for inner in candidates for entry in inner.where_intersect(bboxes[1])), \
+        (entry for inner in probation for entry in inner.where_intersect(bboxes[1]))
 
 
-class IntersectFuzzyInnerAll(IntersectStrategy):
+def intersect_fuzzy_inner_all(outer_idx, bboxes):
     """
-    return iterable inner search result as a whole set with the linear constraints.
+    :param outer_idx: the top level index.
+    :param bboxes: (dim0_min, dim0_max, dim1_min, dim1_max, ...).
+    :return a generator that iterates inner search result as a whole set with the linear constraints.
     """
-    def __init__(self, outer_idx):
-        super().__init__(outer_idx)
-
-    def __call__(self, bboxes):
-        lo, hi = bboxes
-        for outer_key, inner_idx in self._outer:
-            yield inner_idx.where_intersect((lo - outer_key + 1, hi))
+    lo, hi = bboxes
+    for outer_key, inner_idx in outer_idx:
+        yield inner_idx.where_intersect((lo - outer_key + 1, hi))
 
 
-implemented_strategies = ['ReturnEntry', 'FuzzySearch', 'NotSureSearch', 'FuzzyInnerAll']
-strategies = {stra: getattr(sys.modules[__name__], f'Intersect{stra}') for stra in implemented_strategies}
+strategies = {name.removeprefix('intersect_').title().replace('_', ''): stra for name, stra in globals().items()
+              if name.startswith('intersect_')}
