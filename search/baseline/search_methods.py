0f3c4f99fe2d504913d9f08d25617226ecc283c6
Roshall <15504658671@163.com>
2024-01-16 18:57:36 +0800

    FIX BUG

--- search/baseline/search_methods.py
+++ search/baseline/search_methods.py
@@ -9,7 +9,7 @@ from utilities.data_preprocessing import group_by_frame
 def sliding_framework(df, region, labels, duration, interval, *, method='base'):
     frames = group_by_frame(df, interval)
     obj_verifier = partial(obj_verify, labels)
-    windows = sliding_window(frames, duration[0])
+    windows = sliding_window(frames, duration)
     if method == 'base':
         sliding = NaiveSliding(windows, duration, region.enclose, obj_verifier, len_filter, tuple(labels))
     else:
