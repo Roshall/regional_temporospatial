d6749f78ef6e4ea6c4a00989f14f29ce5c6a0a63
Roshall <15504658671@163.com>
2024-01-19 16:41:03 +0800

    make sliding framework consistent with index search

--- search/baseline/search_methods.py
+++ search/baseline/search_methods.py
@@ -7,11 +7,12 @@ from utilities.data_preprocessing import group_by_frame
 
 
 def sliding_framework(df, region, labels, duration, interval, *, method='base'):
+    dur = duration[0]
     frames = group_by_frame(df, interval)
     obj_verifier = partial(obj_verify, labels)
-    windows = sliding_window(frames, duration)
+    windows = sliding_window(frames, dur)
     if method == 'base':
-        sliding = NaiveSliding(windows, duration, region.enclose, obj_verifier, len_filter, tuple(labels))
+        sliding = NaiveSliding(windows, dur, region.enclose, obj_verifier, len_filter, tuple(labels))
     else:
         sliding = None
     return expend(sliding, obj_verifier)
